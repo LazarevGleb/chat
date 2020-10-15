2060f67312bf08666ae79b58f398b47b9e6a810f
LazarevGleb <lazarevgb@gmail.com>
2020-10-15 12:23:37 +0300

    Submit added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {ChatContainerComponent} from './components/chat-container/chat-container.component';
 import {DialogueComponent} from './components/dialogue/dialogue.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import {DialogueComponent} from './components/dialogue/dialogue.component';
     DialogueComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/dialogue/dialogue.component.ts
+++ src/app/components/dialogue/dialogue.component.ts
@@ -1,4 +1,29 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
+import {formatDate} from '@angular/common';
+
+class Message {
+  text: string;
+  date: string;
+  user: User;
+
+
+  constructor(text: string, user: User) {
+    this.text = text;
+    this.user = user;
+    this.date = formatDate(Date.now(), 'hh:mm a | MMM d ', 'en-US', '+0300');
+  }
+}
+
+class User {
+  id: number;
+  name: string;
+
+  constructor(id: number, name: string) {
+    this.id = id;
+    this.name = name;
+  }
+}
 
 @Component({
   selector: 'app-dialogue',
@@ -6,12 +31,33 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./dialogue.component.css']
 })
 export class DialogueComponent implements OnInit {
-  @Input() public name: string;
+  @Input() name: string;
+  messages = new Array<Message>();
+  currentUser: User;
+  messageForm;
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
+    this.messageForm = formBuilder.group({
+      message: ''
+    });
   }
 
   ngOnInit(): void {
+    if (this.name === 'Alica') {
+      this.currentUser = new User(1, 'Alica');
+    } else {
+      this.currentUser = new User(2, 'Jack');
+    }
   }
 
+  onSubmit(messageObject): void {
+    if (messageObject.message) {
+      this.messageForm.reset();
+      this.processMessage(messageObject.message);
+    }
+  }
+
+  processMessage(message: string): void {
+    this.messages.push(new Message(message, this.currentUser));
+  }
 }
